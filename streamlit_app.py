92b65a6f4196dd1e7ee11a7d33b1f5278ebfa106
ReverseAlfred <149822612+ReverseAlfred@users.noreply.github.com>
2023-11-03 16:08:22 -0600

    Update streamlit_app.py
    Update the "" for ''
    Added a text object to emulate a menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title("My Parents New Healty Diner")
+streamlit.title('My Parents New Healty Diner')
 
-streamlit.header("Breakfast Menu")
+streamlit.header('Breakfast Menu')
+streamlit.text('mega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
